ad28e96e4c9b54186d4e95a4197b055c2e861cd9
Domingo <ronin2448@gmail.com>
2021-10-03 17:01:00 -0500

    init commit with transactional

---
+++ spring/core/transactional_pract/transactionalDemo/src/main/java/com/example/transactionalDemo/TransactionalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionalDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalDemoApplication.class, args);
+	}
+
+}
